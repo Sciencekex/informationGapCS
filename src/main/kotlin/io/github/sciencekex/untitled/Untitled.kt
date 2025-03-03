04a767f4dab37469356454b7a5c4539e2654bd83
椎名潤七 <newbingtochatgpt233@outlook.com>
2025-03-03 15:13:38 +0800

    Initial commit

---
+++ src/main/kotlin/io/github/sciencekex/untitled/Untitled.kt
@@ -0,0 +1,14 @@
+package io.github.sciencekex.untitled
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Untitled : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
